24fd3486cc4d3cd20c39569e52934fa41ea371b2
GuillaumeTC1 <gcastelnau@laposte.net>
2025-08-10 00:12:30 +0200

    Add Playnite.FullscreenApp

---
+++ source/Playnite.FullscreenApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Playnite.FullscreenApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
